e031eba55fa93f21c8c59118cd66647b282f09f2
Alvin Mahendra <yusufalvin0799@gmail.com>
2023-05-12 14:22:14 +0700

    updated script package.json

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static('server/public/build'));
 
 // Define a route for the root path
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/build', 'index.html'));
 });
 
 const port = 8001;
